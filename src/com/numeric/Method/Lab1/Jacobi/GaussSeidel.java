eca3e15d401e7f64ddcbc5278c3c93753b543eea
ZanderRak <zanderrak@gmail.com>
2015-06-20 20:15:38 -0700

    Fix code style.

--- src/com/numeric/Method/Lab1/Jacobi/GaussSeidel.java
+++ src/com/numeric/Method/Lab1/Jacobi/GaussSeidel.java
@@ -3,98 +3,102 @@ package com.numeric.Method.Lab1.Jacobi;
 import java.util.Arrays;
 
 public class GaussSeidel {
-
     public static final int MAX_ITERATIONS = 1000;
-    private double[][] M;
+    private double[][] matrix;
     public GaussSeidel(double[][] matrix) {
-        M = matrix;
+        this.matrix = matrix;
     }
+
     public void print() {
-        int n = M.length;
-        for (double[] aM : M) {
-            for (int j = 0; j < n + 1; j++)
-                System.out.print(aM[j] + " ");
+        for (double[] matrixElement : matrix) {
+            for (int j = 0; j < matrix.length + 1; j++)
+                System.out.print(matrixElement[j] + " ");
             System.out.println();
         }
     }
+
     public boolean transformToDominant(int r, boolean[] V, int[] R) {
-        int n = M.length;
-        if (r == M.length) {
-            double[][] T = new double[n][n + 1];
+        if (r == matrix.length) {
+            double[][] matrixTransposable = new double[matrix.length][matrix.length + 1];
             for (int i = 0; i < R.length; i++) {
-                System.arraycopy(M[R[i]], 0, T[i], 0, n + 1);
+                System.arraycopy(matrix[R[i]], 0, matrixTransposable[i], 0, matrix.length + 1);
             }
-
-            M = T;
-
+            matrix = matrixTransposable;
             return true;
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             if (V[i]) continue;
-
             double sum = 0;
+            for (int j = 0; j < matrix.length; j++)
+                sum += Math.abs(matrix[i][j]);
 
-            for (int j = 0; j < n; j++)
-                sum += Math.abs(M[i][j]);
-
-            if (2 * Math.abs(M[i][r]) > sum) { // diagonally dominant?
+            if (2 * Math.abs(matrix[i][r]) > sum) { // diagonally dominant?
                 V[i] = true;
                 R[r] = i;
 
                 if (transformToDominant(r + 1, V, R))
                     return true;
-
                 V[i] = false;
             }
         }
-
         return false;
     }
+
     public boolean makeDominant() {
-        boolean[] visited = new boolean[M.length];
-        int[] rows = new int[M.length];
+        boolean[] visited = new boolean[matrix.length];
+        int[] rows = new int[matrix.length];
 
         Arrays.fill(visited, false);
 
         return transformToDominant(0, visited, rows);
     }
+
     public void solve() {
         int iterations = 0;
-        int n = M.length;
+        int n = matrix.length;
         double epsilon = 0.1;
-        double[] X = new double[n]; // Approximations
-        double[] P = new double[n]; // Prev
-        Arrays.fill(X, 0);
+        double[] approximations = new double[n]; // Approximations
+        double[] prev = new double[n]; // Prev
+        Arrays.fill(approximations, 0);
 
         while (true) {
             for (int i = 0; i < n; i++) {
-                double sum = M[i][n]; // b_n
+                double sum = matrix[i][n]; // b_n
 
-                for (int j = 0; j < n; j++)
-                    if (j != i)
-                        sum -= M[i][j] * X[j];
-
+                for (int j = 0; j < n; j++) {
+                    if (j != i) {
+                        sum -= matrix[i][j] * approximations[j];
+                    }
+                }
                 // Update x_i to use in the next row calculation
-                X[i] = 1 / M[i][i] * sum;
+                approximations[i] = 1 / matrix[i][i] * sum;
             }
 
             System.out.print("X_" + iterations + " = {");
-            for (int i = 0; i < n; i++)
-                System.out.print(X[i] + " ");
+
+            for (int i = 0; i < n; i++) {
+                System.out.print(approximations[i] + " ");
+            }
+
             System.out.println("}");
 
             iterations++;
-            if (iterations == 1)
+            if (iterations == 1) {
                 continue;
+            }
 
             boolean stop = true;
-            for (int i = 0; i < n && stop; i++)
-                if (Math.abs(X[i] - P[i]) > epsilon)
+            for (int i = 0; i < n && stop; i++) {
+                if (Math.abs(approximations[i] - prev[i]) > epsilon) {
                     stop = false;
+                }
+            }
 
-            if (stop || iterations == MAX_ITERATIONS) break;
-            P = X.clone();
+            if (stop || iterations == MAX_ITERATIONS) {
+                break;
+            }
+            prev = approximations.clone();
         }
     }
 }
